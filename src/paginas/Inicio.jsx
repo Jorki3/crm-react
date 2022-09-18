376ad09421b6728315e6f8fe85b7a7ddbd62fce1
Jorki3 <eduardomttzt01@outlook.com>
2022-09-18 02:02:17 -0500

    Leer clientes

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Inicio = () => {
+  const [clientes, setClientes] = useState([])
+
+  useEffect(() => {
+    const obtenerClientesAPI = async () => {
+      try {
+        const url = 'http://localhost:3000/clientes'
+
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+
+        setClientes(resultado);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    obtenerClientesAPI()
+  }, [])
+
+
   return (
     <div>Inicio</div>
   )
