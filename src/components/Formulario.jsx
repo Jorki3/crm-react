d3ef33e750f68c6fb8ac6dfdb1394dd5665cf39a
Jorki3 <eduardomttzt01@outlook.com>
2022-09-18 00:46:49 -0500

    Leer Valores

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { Formik, Form, Field } from 'formik'
 
 const Formulario = () => {
+    const handleSubmit = (values) => {
+        console.log(values);
+    }
+
     return (
         <div className='bg-white mt-10 px-5 py-10 rounded-md shadow-md 
     md:w-3/4 mx-auto'>
@@ -14,6 +18,10 @@ const Formulario = () => {
                     email: '',
                     telefono: '',
                     notas: ''
+                }}
+
+                onSubmit={(values) => {
+                    handleSubmit(values)
                 }}
             >
 
