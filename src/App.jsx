f6c0c235314cc669d4c8922f171ff4e27b7163bb
Jorki3 <eduardomttzt01@outlook.com>
2022-09-19 12:10:58 -0500

    uso deENV

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import EditarCliente from './paginas/EditarCliente'
 import VerCliente from './paginas/VerCliente'
 
 function App() {
+  console.log(import.meta.env);
+
   return (
     <BrowserRouter>
       <Routes>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -28,7 +28,7 @@ const Formulario = ({ cliente, cargando }) => {
 
             if (cliente.id) {
                 // Editar registro
-                const url = `http://localhost:3000/clientes/${cliente.id}`
+                const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`
 
                 respuesta = await fetch(url, {
                     method: 'PUT',
@@ -39,7 +39,7 @@ const Formulario = ({ cliente, cargando }) => {
                 })
             } else {
                 // Crear registro
-                const url = 'http://localhost:3000/clientes'
+                const url = import.meta.env.VITE_API_URL
 
                 respuesta = await fetch(url, {
                     method: 'POST',
